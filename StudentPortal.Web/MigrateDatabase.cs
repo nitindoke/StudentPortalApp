15afda3a24356209134e21c29f9e95d26857ab1c
nitin doke <nitin123.doke@gmail.com>
2026-05-13 11:02:33 +0530

    Added Migration logic

---
+++ StudentPortal.Web/MigrateDatabase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Web
+{
+    public static class MigrateDatabase
+    {
+        public static IHost Migrate<T>(this IHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            return webHost;
+        }
+    }
+}
--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web;
 using StudentPortal.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,28 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal"))
 //builder.Services.AddSingleton<ApplicationDbContext, ApplicationDbContext>();
 //builder.Services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
 //builder.Services.AddTransient<ApplicationDbContext, ApplicationDbContext>();
+
 var app = builder.Build();
+
+var host = app.Migrate<ApplicationDbContext>();
+
+// migrate the database.  Best practice = in Main, using service scope
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+        // for demo purposes, delete the database & migrate on startup so 
+        // we can start with a clean slate
+        //context.Database.EnsureDeleted();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
